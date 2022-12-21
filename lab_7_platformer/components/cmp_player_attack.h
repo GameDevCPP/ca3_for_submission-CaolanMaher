009cc11687ba061adb6c921668ee9b9f0b740e20
CaolanMaher <caolanmaher2@gmail.com>
2022-12-21 11:37:14 +0000

    Added Attack Components

---
+++ lab_7_platformer/components/cmp_player_attack.h
@@ -0,0 +1,14 @@
+//
+// Created by caola on 21/12/2022.
+//
+
+#ifndef GAMES_ENGINEERING_CMP_PLAYER_ATTACK_H
+#define GAMES_ENGINEERING_CMP_PLAYER_ATTACK_H
+
+
+class cmp_player_attack {
+
+};
+
+
+#endif //GAMES_ENGINEERING_CMP_PLAYER_ATTACK_H
